ffce83daebf58a7b84f4f0ac8f4d115e7d61e1d0
Felipe Brito <felipefjbrito@gmail.com>
2026-09-07 18:12:54 -0700

    inplement homepage

---
+++ constants/images.ts
@@ -0,0 +1,5 @@
+import splathPattern from '@/assets/images/splash-pattern.png'
+// eslint-disable-next-line import/no-unresolved
+import avatar from '@/assets/images/avatar.png'
+
+export default {splathPattern, avatar}
